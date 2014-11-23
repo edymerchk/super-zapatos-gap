fbd726e580ed421623889bbf508a3a5c62ec8b08
Luis Edimerchk Laverde <edyhack@gmail.com>
2014-11-23 18:21:00 -0500

    Adding HTTP basic auth

--- app/controllers/api/defaults.rb
+++ app/controllers/api/defaults.rb
@@ -8,6 +8,11 @@ module API
       rescue_from ActiveRecord::RecordNotFound do |e|
         rack_response('{ "success": false, "error_msg": "Record not Found", "error_code": 404 }', 404)
       end
+
+      # HTTP Basic Authentication
+      http_basic do |username, password|
+        username ==  "my_username"  && password == "my_password"
+      end
     end
   end
 end
