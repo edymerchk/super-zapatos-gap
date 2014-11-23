fcbcbcba879b3dd96bdb162b765aecf3e99f7377
Luis Edimerchk Laverde <edyhack@gmail.com>
2014-11-23 00:45:27 -0500

    Adding sample fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do
+  store = Store.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address
+  )
+  2.times do
+    Article.create!(
+      name: Faker::Commerce.product_name,
+      description: Faker::Lorem.sentence,
+      price: rand(100..200),
+      total_in_vault: rand(1..100),
+      total_in_shelf: rand(1..100),
+      store_id: store.id
+    )
+  end
+end
